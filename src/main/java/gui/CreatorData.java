a1750312a61306283e10fc127a499891732e6c1c
Jakub Dzieciątko <jdzieciatko@gmail.com>
2020-06-20 16:42:02 +0200

    Second screen completed

--- src/main/java/gui/CreatorData.java
+++ src/main/java/gui/CreatorData.java
@@ -1,12 +1,27 @@
 package gui;
 
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreatorData {
+    // creator page 1
     private String clientName;
     private String clientSurname;
     private String clientPhoneNumber;
     private LocalDate funeralDate;
+    // creator page 2
+    private boolean standardCoffinSelected = true;
+    private boolean premiumCoffinSelected;
+    private List<String> selectedDecorations = new ArrayList<>();
+    private String deadmanName;
+    private String deadmanSurname;
+    private List<FuneralCreator2.TableData> selectedSets = new ArrayList<>();
 
     public String getClientName() {
         return clientName;
@@ -38,5 +53,53 @@ public class CreatorData {
 
     public void setFuneralDate(LocalDate funeralDate) {
         this.funeralDate = funeralDate;
+    }
+
+    public boolean isStandardCoffinSelected() {
+        return standardCoffinSelected;
+    }
+
+    public void setStandardCoffinSelected(boolean standardCoffinSelected) {
+        this.standardCoffinSelected = standardCoffinSelected;
+    }
+
+    public boolean isPremiumCoffinSelected() {
+        return premiumCoffinSelected;
+    }
+
+    public void setPremiumCoffinSelected(boolean premiumCoffinSelected) {
+        this.premiumCoffinSelected = premiumCoffinSelected;
+    }
+
+    public List<String> getSelectedDecorations() {
+        return selectedDecorations;
+    }
+
+    public void setSelectedDecorations(List<String> selectedDecorations) {
+        this.selectedDecorations = selectedDecorations;
+    }
+
+    public String getDeadmanName() {
+        return deadmanName;
+    }
+
+    public void setDeadmanName(String deadmanName) {
+        this.deadmanName = deadmanName;
+    }
+
+    public String getDeadmanSurname() {
+        return deadmanSurname;
+    }
+
+    public void setDeadmanSurname(String deadmanSurname) {
+        this.deadmanSurname = deadmanSurname;
+    }
+
+    public List<FuneralCreator2.TableData> getSelectedSets() {
+        return selectedSets;
+    }
+
+    public void setSelectedSets(List<FuneralCreator2.TableData> selectedSets) {
+        this.selectedSets = selectedSets;
     }
 }
--- src/main/java/gui/FuneralCreator2.java
+++ src/main/java/gui/FuneralCreator2.java
@@ -1,13 +1,56 @@
 package gui;
 
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
 
 public class FuneralCreator2 extends FuneralCreatorBase {
+    public class TableData {
+        public SimpleStringProperty coffinType = new SimpleStringProperty();
+        public SimpleListProperty<String> decorations = new SimpleListProperty<>();
+        public SimpleStringProperty deadmanName = new SimpleStringProperty();
+        public SimpleStringProperty deadmanSurname = new SimpleStringProperty();
+
+        public String getCoffinType() {
+            return coffinType.get();
+        }
+
+        public ObservableList<String> getDecorations() {
+            return decorations.get();
+        }
+
+        public String getDeadmanName() {
+            return deadmanName.get();
+        }
+
+        public String getDeadmanSurname() {
+            return deadmanSurname.get();
+        }
+    }
+
+    // table data
+    private ObservableList<TableData> tableData;
+
     @FXML
-    private ListView<String> selectedSetsListView;
+    private TableView<TableData> selectedSetsTableView;
+
+    @FXML
+    private TableColumn<TableData, String> deadmanNameColumn;
+
+    @FXML
+    private TableColumn<TableData, String> deadmanSurnameColumn;
+
+    @FXML
+    private TableColumn<TableData, String> coffinTypeColumn;
+
+    @FXML
+    private TableColumn<TableData, List<String>> decorationsColumn;
 
     @FXML
     private ListView<String> availableDecorationsListView;
@@ -21,19 +64,124 @@ public class FuneralCreator2 extends FuneralCreatorBase {
     @FXML
     private Button nextButton;
 
+    @FXML
+    private Button addDecorationButton;
+
+    @FXML
+    private Button removeDecorationButton;
+
+    @FXML
+    private Button addOrderButton;
+
+    @FXML
+    private Button removeOrderButton;
+
+    @FXML
+    private RadioButton standardCoffinRadioButton;
+
+    @FXML
+    private RadioButton premiumCoffinRadioButton;
+
+    @FXML
+    private TextField deadmanName;
+
+    @FXML
+    private TextField deadmanSurname;
+
     public void initialize() {
         backButton.setOnAction(e -> {
+            saveData();
             setView("/funeralcreator1.fxml");
         });
         nextButton.setOnAction(e -> {
+            saveData();
             setView("/funeralcreator3.fxml");
         });
-        selectedSetsListView.setItems(FXCollections.observableArrayList());
-        availableDecorationsListView.setItems(FXCollections.observableArrayList("Dekoracja 1", "Dekoracja 2"));
-        selectedDecorationsListView.setItems(FXCollections.observableArrayList("Dekoracja 3"));
+        standardCoffinRadioButton.setOnAction(e -> {
+            var rb = (RadioButton)e.getSource();
+            if (rb.isSelected()) {
+                setDisableDecorationSelection(true);
+            }
+        });
+        premiumCoffinRadioButton.setOnAction(e -> {
+            var rb = (RadioButton)e.getSource();
+            if (rb.isSelected()) {
+                setDisableDecorationSelection(false);
+            }
+        });
+        addDecorationButton.setOnAction(e -> {
+            var items = availableDecorationsListView.getSelectionModel().getSelectedItems();
+            selectedDecorationsListView.getItems().addAll(items);
+        });
+        removeDecorationButton.setOnAction(e -> {
+            var items = selectedDecorationsListView.getSelectionModel().getSelectedItems();
+            selectedDecorationsListView.getItems().removeAll(items);
+        });
+        addOrderButton.setOnAction(e -> {
+            var item = new TableData();
+            item.deadmanName.setValue(deadmanName.getText());
+            item.deadmanSurname.setValue(deadmanSurname.getText());
+            item.coffinType.setValue(premiumCoffinRadioButton.isSelected() ? "premium" : "standard");
+            item.decorations.setValue(premiumCoffinRadioButton.isSelected() ? selectedDecorationsListView.getItems() : null);
+            tableData.add(item);
+            clearForm();
+        });
+        removeOrderButton.setOnAction(e -> {
+            var selected = selectedSetsTableView.getSelectionModel().getSelectedItem();
+            selectedSetsTableView.getItems().removeAll(selected);
+        });
+        availableDecorationsListView.setItems(FXCollections.observableArrayList("Dekoracja 1", "Dekoracja 2", "Dekoracja 3"));
+
+        deadmanNameColumn.setCellValueFactory(new PropertyValueFactory<>("deadmanName"));
+        deadmanSurnameColumn.setCellValueFactory(new PropertyValueFactory<>("deadmanSurname"));
+        coffinTypeColumn.setCellValueFactory(new PropertyValueFactory<>("coffinType"));
+        decorationsColumn.setCellValueFactory(new PropertyValueFactory<>("decorations"));
+        tableData = FXCollections.observableArrayList();
+        selectedSetsTableView.setItems(tableData);
+    }
+
+    /**
+     * Disables or enables decoration selection views
+     * @param value disables if set to false
+     */
+    private void setDisableDecorationSelection(boolean value) {
+        availableDecorationsListView.setDisable(value);
+        selectedDecorationsListView.setDisable(value);
+        addDecorationButton.setDisable(value);
+        removeDecorationButton.setDisable(value);
+    }
+
+    /**
+     * Clears set configuration form (name and surname).
+     */
+    private void clearForm() {
+        deadmanName.setText("");
+        deadmanSurname.setText("");
+    }
+
+    private void saveData() {
+        this.creatorData.setStandardCoffinSelected(standardCoffinRadioButton.isSelected());
+        this.creatorData.setPremiumCoffinSelected(premiumCoffinRadioButton.isSelected());
+        this.creatorData.setSelectedDecorations(selectedDecorationsListView.getItems());
+        this.creatorData.setDeadmanName(deadmanName.getText());
+        this.creatorData.setDeadmanSurname(deadmanSurname.getText());
+        this.creatorData.setSelectedSets(tableData);
     }
 
     public void initData(CreatorData data) {
         super.initData(data);
+        // restore data
+        standardCoffinRadioButton.setSelected(this.creatorData.isStandardCoffinSelected());
+        if (this.creatorData.isStandardCoffinSelected()) {
+            setDisableDecorationSelection(true);
+        }
+        premiumCoffinRadioButton.setSelected(this.creatorData.isPremiumCoffinSelected());
+        if (this.creatorData.isPremiumCoffinSelected()) {
+            setDisableDecorationSelection(false);
+        }
+        selectedDecorationsListView.setItems(FXCollections.observableArrayList(this.creatorData.getSelectedDecorations()));
+        deadmanName.setText(this.creatorData.getDeadmanName());
+        deadmanSurname.setText(this.creatorData.getDeadmanSurname());
+        tableData.addAll(this.creatorData.getSelectedSets());
     }
 }
