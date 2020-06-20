b4ab42065f1718b99150c1a9b023352b2c3d6b1d
Jakub Dzieciątko <jdzieciatko@gmail.com>
2020-06-20 13:23:56 +0200

    Fix responsiveness

--- src/main/java/gui/ControllerBase.java
+++ src/main/java/gui/ControllerBase.java
@@ -2,8 +2,11 @@ package gui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -13,8 +16,8 @@ public class ControllerBase {
 
     protected void setView(String path) {
         try {
-            var pane = FXMLLoader.load(getClass().getResource(path));
-            rootPane.getChildren().setAll((Node) pane);
+            var pane = (AnchorPane)FXMLLoader.load(getClass().getResource(path));
+            rootPane.getChildren().setAll(pane);
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,17 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-
-import javax.swing.*;
-import java.io.IOException;
 
 public class Main extends Application {
     public static StandardServiceRegistry registry = null;
